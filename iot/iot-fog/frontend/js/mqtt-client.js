0e69679cc0997386e6db3e6582088e66cd387c08
ShelsyMartinez <158532397+ShelsyMartinez@users.noreply.github.com>
2025-07-23 15:24:24 -0500

    Update mqtt-client.js

--- iot/iot-fog/frontend/js/mqtt-client.js
+++ iot/iot-fog/frontend/js/mqtt-client.js
@@ -1,14 +1,13 @@
 // Configura conexión
-const brokerUrl = 'ws://3.16.48.213:8083/mqtt'; // WebSocket listener de EMQX
-const mqttUser = 'SHELSY';      // Usa el usuario MQTT válido
-const mqttPassword = '123456789'; // Contraseña del usuario
+const brokerUrl = 'ws://3.16.48.213:8083/mqtt';
+const token = localStorage.getItem('token');
 
 const options = {
   clean: true,
   connectTimeout: 4000,
   clientId: 'web-client-' + Math.floor(Math.random() * 1000),
-  username: mqttUser,
-  password: mqttPassword
+  username: token,
+  password: ''
 };
 
 const client = mqtt.connect(brokerUrl, options);
